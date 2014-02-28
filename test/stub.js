34bdd57bc9d97fd99a1c221e63c871b287f31ac7
mhhf <mhhf@freenet.de>
2014-02-28 15:35:20 +0100

    [+] added tests for new syntax

--- test/stub.js
+++ test/stub.js
@@ -19,22 +19,22 @@ describe('slide Parser', function(){
   describe('??? blocks', function(){
     
     it('should find empty blocks', function() {
-      parser.parse('???\n???\n').should.deep.equal([{exp:[]}]);
+      parser.parse('{{#???}}\n{{/???}}\n').should.deep.equal([{exp:[]}]);
     });
     
     it('should ignore whitespace', function() {
-      parser.parse('???  \n???\n').should.deep.equal([{exp:[]}]);
-      parser.parse('???\n???  \n').should.deep.equal([{exp:[]}]);
-      parser.parse('???\t\n???\n').should.deep.equal([{exp:[]}]);
-      parser.parse('???\n???\t\n').should.deep.equal([{exp:[]}]);
+      parser.parse('{{#???}}  \n{{/???}}\n').should.deep.equal([{exp:[]}]);
+      parser.parse('{{#???}}\n{{/???}}  \n').should.deep.equal([{exp:[]}]);
+      parser.parse('{{#???}}\t\n{{/???}}\n').should.deep.equal([{exp:[]}]);
+      parser.parse('{{#???}}\n{{/???}}\t\n').should.deep.equal([{exp:[]}]);
     });
     
     it('should find full blocks', function() {
-      parser.parse('???\nasd\n???\n').should.deep.equal([{exp:["asd"]}]);
+      parser.parse('{{#???}}\nasd\n{{/???}}\n').should.deep.equal([{exp:["asd"]}]);
     });
     
     it('should ignore empty lines', function() {
-      parser.parse('???\nasd\n\nasd\n???\n').should.deep.equal([{exp:["asd","asd"]}]);
+      parser.parse('{{#???}}\nasd\n\nasd\n{{/???}}\n').should.deep.equal([{exp:["asd","asd"]}]);
     });
     
   })
@@ -54,24 +54,24 @@ describe('slide Parser', function(){
   describe('package blocks', function(){
     
     it('should find them', function() {
-      parser.parse('{{ packageName }}\n').should.deep.equal([{
+      parser.parse('{{> packageName }}\n').should.deep.equal([{
         "package": "packageName",
         "data": ""
       }]);
     });
     
     it('should find package data', function() {
-      parser.parse('{{ packageName {} }}\n').should.deep.equal([{
+      parser.parse('{{> packageName {} }}\n').should.deep.equal([{
         "package": "packageName",
         "data": " {} "
       }]);
       
-      parser.parse('{{ packageName {}}}\n').should.deep.equal([{
+      parser.parse('{{> packageName {}}}\n').should.deep.equal([{
         "package": "packageName",
         "data": " {}"
       }]);
       
-      parser.parse('{{ packageName \n{\n}\n}}\n').should.deep.equal([{
+      parser.parse('{{> packageName \n{\n}\n}}\n').should.deep.equal([{
         "package": "packageName",
         "data": " \n{\n}\n"
       }]);
@@ -79,7 +79,7 @@ describe('slide Parser', function(){
     
     it('should find packageData nested json', function() {
       
-      parser.parse('{{ packageName {a:{b:{c:"d"}}}}}\n').should.deep.equal([{
+      parser.parse('{{> packageName {a:{b:{c:"d"}}}}}\n').should.deep.equal([{
         "package": "packageName",
         "data": ' {a:{b:{c:"d"}}}'
       }]);
@@ -103,15 +103,15 @@ describe('slide Parser', function(){
   describe('block interaction', function(){
     
     it('should find multiple blocks md,exp', function() {
-      parser.parse('markdown1\n???\nexp\n???\n').should.deep.equal([{md:"markdown1\n"},{exp:["exp"]}]);
+      parser.parse('markdown1\n{{#???}}\nexp\n{{/???}}\n').should.deep.equal([{md:"markdown1\n"},{exp:["exp"]}]);
     });
     
     it('should find multiple blocks exp,md', function() {
-      parser.parse('???\nexp\n???\nmarkdown1\n').should.deep.equal([{exp:["exp"]},{md:"markdown1\n"}]);
+      parser.parse('{{#???}}\nexp\n{{/???}}\nmarkdown1\n').should.deep.equal([{exp:["exp"]},{md:"markdown1\n"}]);
     });
     
     it('should find multiple blocks exp,md,exp,md', function() {
-      parser.parse('???\nexp\n???\nmarkdown1\n???\nexp2\n???\nmarkdown2\n').should.deep.equal([{exp:["exp"]},{md:"markdown1\n"},{exp:["exp2"]},{md:"markdown2\n"}]);
+      parser.parse('{{#???}}\nexp\n{{/???}}\nmarkdown1\n{{#???}}\nexp2\n{{/???}}\nmarkdown2\n').should.deep.equal([{exp:["exp"]},{md:"markdown1\n"},{exp:["exp2"]},{md:"markdown2\n"}]);
     });
     
   });
